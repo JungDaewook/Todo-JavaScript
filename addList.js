fa8fdc1c64e94acac6a79efb933b90e720a243a5
정대욱 <jungdaewook@jeongdaeug-ui-MacBookPro.local>
2022-03-23 00:34:49 +0900

    modify branch name and html, javascript code by unpo88's coment

--- addList.js
+++
@@ -1,8 +0,0 @@
-function addList()  {
-    const addValue = document.getElementById('addValue').value;
-    const li = document.createElement('li');
-    li.setAttribute('id',addValue);
-    const textNode = document.createTextNode(addValue);
-    li.appendChild(textNode);
-    document.getElementById('list').appendChild(li);
-  }
