45c943da5cd9e1a474a915b6ac669e9f3abf703c
정대욱 <jungdaewook@jeongdaeug-ui-MacBookPro.local>
2022-03-27 21:44:33 +0900

    modify by code review

---
+++ addTodoItem.js
@@ -0,0 +1,8 @@
+function addTodoItem()  {
+  const todoInputText = document.getElementById('todoInputText').value;
+  const todoItem = document.createElement('li');
+  const textNode = document.createTextNode(todoInputText);
+  todoItem.appendChild(textNode);
+  todoList = document.getElementById('todoList');
+  todoList.appendChild(todoItem);
+}
