1c6659f3cb3acd66b577c2e3f11a5de998c10663
정대욱 <jungdaewook@jeongdaeug-ui-MacBookPro.local>
2022-04-18 23:52:43 +0900

    코멘트에 따라 MAX_TODO_ITEM_COUNT, EMPTY_TODO_ITEM_COUNT 변수 생성, TodoItemCount에서 TODO_ITEM_COUNT로 변수명 변경, isEmptyTodoInputText(), isBiggerThanMAX_TODO_ITEM_COUNT 함수 생성 1.MAX_TODO_ITEM_COUNT, EMPTY_TODO_ITEM_COUNT 변수 생성 2. TodoItemCount -> TODO_ITEM_COUNT 로 변수명 변경 3. srp 원칙에 의거, isEmptyTodoInputText(), isBiggerThanMAX_TODO_ITEM_COUNT() 생성 4. addTodoItem()에서 isEmptyTodoInputText()를 isBiggerThanMAX_TODO_ITEM_COUNT() 보다 먼저 오도록 변경(빈 입력값이 들어가있는지 확인해주는게 우선순위가 높다고 판단했기 때문)

--- functionsOfTodoList.js
+++ functionsOfTodoList.js
@@ -1,4 +1,7 @@
-let TodoItemCount = 0;
+const MAX_TODO_ITEM_COUNT = 20;
+const EMPTY_TODO_ITEM = '';
+let TODO_ITEM_COUNT = 0;
+
 
 function getTodoInputTextValue() {
   return document.getElementById('todoInputText').value;
@@ -8,26 +11,42 @@ function addTodoItem()  {
   const todoItem = document.createElement('li');
   const textNode = document.createTextNode(getTodoInputTextValue());
 
-  if (TodoItemCount >= 20){
-    return ;
-  }
-
-  if(getTodoInputTextValue() === ''){
+  if(isEmptyTodoInputText()){
     return;
   }
 
-  todoItem.setAttribute("id", "todoItem" + TodoItemCount);
+  if (isBiggerThanMAX_TODO_ITEM_COUNT()){
+    return ;
+  }
+
+  todoItem.setAttribute("id", "todoItem" + TODO_ITEM_COUNT);
   todoItem.appendChild(textNode);
-  todoItem.innerHTML += "<button type = 'button' onclick = 'removeTodoItem("+TodoItemCount+")')> 삭제 </button>";
+  todoItem.innerHTML += "<button type = 'button' onclick = 'removeTodoItem("+TODO_ITEM_COUNT+")')> 삭제 </button>";
   
   todoList = document.getElementById('todoList');
   todoList.appendChild(todoItem);
-  TodoItemCount++;
+  TODO_ITEM_COUNT++;
   
   document.getElementById('todoInputText').value = '';
 }
 
-function removeTodoItem(TodoItemCount) {
-  const deleteTodoItem = document.getElementById('todoItem' + TodoItemCount);
+function removeTodoItem(TODO_ITEM_COUNT) {
+  const deleteTodoItem = document.getElementById('todoItem' + TODO_ITEM_COUNT);
   todoList.removeChild(deleteTodoItem);
+}
+
+function isBiggerThanMAX_TODO_ITEM_COUNT(){
+  if (TODO_ITEM_COUNT >= MAX_TODO_ITEM_COUNT){
+    alert("할 일은 20개까지만 입력할 수 있습니다.");
+    return true;
+  }
+  return false;
+}
+
+function isEmptyTodoInputText(){
+  if (getTodoInputTextValue() === EMPTY_TODO_ITEM){
+    alert("입력창에 할 일을 입력하십시오.");
+    return true;
+  }
+  return false;
 }
