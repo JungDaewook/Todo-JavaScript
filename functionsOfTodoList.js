5ae9f866361f954f4b4b392ed2d1581f2a191fa5
정대욱 <jungdaewook@jeongdaeug-ui-MacBookPro.local>
2022-06-09 00:55:38 +0900

    가독성을 위해 innerHTML 코드를 여러줄로 구별.

--- functionsOfTodoList.js
+++ functionsOfTodoList.js
@@ -13,7 +13,8 @@ function addTodoItem()  {
 
   todoItem.setAttribute("id", todoItemId);
   todoItem.appendChild(textNode);
-  todoItem.innerHTML += "&nbsp; <input type = 'button' id = 'strikeButton' onclick = 'strikeTodoItem(\""+todoItemId+"\")' value = '완료'/> <input type = 'button' onclick = 'removeTodoItem(\""+todoItemId+"\")' value = '삭제'/>";
+  todoItem.innerHTML += "&nbsp; <input type = 'button' id = 'strikeButton' onclick = 'strikeTodoItem(\""+todoItemId+"\")' value = '완료'/>"; 
+  todoItem.innerHTML += "<input type = 'button' onclick = 'removeTodoItem(\""+todoItemId+"\")' value = '삭제'/>";
 
   todoList = document.getElementById('todoList');
   todoList.appendChild(todoItem);
@@ -26,12 +27,18 @@ function addTodoItem()  {
 
 function strikeTodoItem(todoItemId) {
   const strikeTodoItem = document.getElementById(todoItemId);
-  strikeTodoItem.innerHTML = strikeTodoItem.innerText.strike() + "<input type = 'button' id = 'strikeButton' onclick = 'cancelStrikeTodoItem(\""+todoItemId+"\")' value = '취소'/> <input type = 'button' onclick = 'removeTodoItem(\""+todoItemId+"\")' value = '삭제'/>";
+
+  strikeTodoItem.innerHTML = strikeTodoItem.innerText.strike();
+  strikeTodoItem.innerHTML += "<input type = 'button' id = 'strikeButton' onclick = 'cancelStrikeTodoItem(\""+todoItemId+"\")' value = '취소'/>";
+  strikeTodoItem.innerHTML += "<input type = 'button' onclick = 'removeTodoItem(\""+todoItemId+"\")' value = '삭제'/>";
 }
 
 function cancelStrikeTodoItem(todoItemId) {
   const cancelStrikeTodoItem = document.getElementById(todoItemId);
-  cancelStrikeTodoItem.innerHTML = cancelStrikeTodoItem.innerText + "<input type = 'button' id = 'strikeButton' onclick = 'strikeTodoItem(\""+todoItemId+"\")' value = '완료'/> <input type = 'button' onclick = 'removeTodoItem(\""+todoItemId+"\")' value = '삭제'/>";
+  
+  cancelStrikeTodoItem.innerHTML = cancelStrikeTodoItem.innerText;
+  cancelStrikeTodoItem.innerHTML += "<input type = 'button' id = 'strikeButton' onclick = 'strikeTodoItem(\""+todoItemId+"\")' value = '완료'/>";
+  cancelStrikeTodoItem.innerHTML += "<input type = 'button' onclick = 'removeTodoItem(\""+todoItemId+"\")' value = '삭제'/>";
 }
 
 function removeTodoItem(todoItemId) {
